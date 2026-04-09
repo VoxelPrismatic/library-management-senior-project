f569bff571d99dc9ebfc911d079754e5fa05b266
VoxelPrismatic <voxelprismatic@pm.me>
2026-04-09 18:30:49 -0500

    add fields based on PR discussion
    Signed-off-by: VoxelPrismatic <voxelprismatic@pm.me>

--- common/fine.go
+++ common/fine.go
@@ -6,24 +6,44 @@ import (
 	"gorm.io/gorm"
 )
 
+// A fine that must be paid before the user can check out more books.
+// A fine can be issued for any reason, including late, lost, or damaged.
 type Fine struct {
 	gorm.Model
-	ID             int `gorm:"primaryKey"`
-	UserID         int
-	UserRef        User
-	LoanID         int
-	LoanRef        Loan
-	AmountAssessed float32
-	AmountPaid     float32
-	Status         FineStatus
-	LastPayment    time.Time
+	ID      int `gorm:"primaryKey"`
+	UserID  int
+	UserRef User
+	LoanID  int
+	LoanRef Loan
+
+	IssueReason     FineReasonFlag
+	IssueDate       time.Time
+	AmountIssued    float32
+	AmountRemaining float32 // How much is remaining, recalculated after every transaction
+
+	AmountWaived  float32 // Any discounts provided by a librarian
+	WaivedReasion string
+	WaivedBy      string
 }
 
-type FineStatus int
+// Transactions resolve the oldest fines first
+/* TO-DO: Figure out how to link transactions to specific fines
+ *        - Do we split one transaction into its components?
+ *        - Do we not care?
+ *        - Do we include a list of Fine IDs?
+ */
+type Transaction struct {
+	gorm.Model
+	UserID     int
+	UserRef    User
+	AmountPaid float32
+	Date       time.Time
+}
+
+type FineReasonFlag int
 
 const (
-	FineStatusUnpaid FineStatus = iota
-	FineStatusPartial
-	FineStatusPaid
-	FineStatusWaived
+	FineReasonLate FineReasonFlag = iota
+	FineReasonLost
+	FineReasonDamaged
 )
