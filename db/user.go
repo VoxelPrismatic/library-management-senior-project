0e50bbd226213e24b469b8e2282305c978a121c1
VoxelPrismatic <voxelprismatic@pm.me>
2026-04-20 11:47:48 -0500

    add user database shorthands

--- db/user.go
+++ db/user.go
@@ -1,5 +1,7 @@
 package db
 
+import "time"
+
 var _ = Migrate(User{})
 
 type User struct {
@@ -36,6 +38,25 @@ type UserStatusFlag int
 
 const (
 	UserStatusActive  UserStatusFlag = 1 << iota
+	UserStatusLimited                // User has hit loan limit
 	UserStatusLocked                 // TO-DO: Check if user has outstanding fees and remove this redundant lock
 	UserStatusDeleted                // For audit purposes; we may choose to anonymize any data
 )
+
+func (u User) CheckedOut() ([]Loan, error) {
+	ret := []Loan{}
+	status := db.Model(&Loan{}).Joins("users").Where(
+		"date_returned = ?", NilTime,
+	).Find(&ret)
+	return ret, status.Error
+}
+
+func (u User) HasOverdueBooks() (bool, error) {
+	count := int64(0)
+	status := db.Model(&Loan{}).Joins("users").Where(
+		"date_returned = ?", NilTime,
+	).Where(
+		"date_checkout < ?", time.Now().Add(-LOAN_DURATION),
+	).Count(&count)
+	return count == 0, status.Error
+}
