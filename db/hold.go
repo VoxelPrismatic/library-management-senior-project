e5bfa37b2f6176103b7e8dd93a59b5e9b6b2b205
VoxelPrismatic <voxelprismatic@pm.me>
2026-04-20 11:48:01 -0500

    add hold database shorthands

--- db/hold.go
+++ db/hold.go
@@ -7,11 +7,13 @@ var _ = Migrate(Hold{})
 // When a user wants to get in line to check out a book
 type Hold struct {
 	BaseModel
-	WorkID        SqlUUID `gorm:"type:text"`
+	BookWork      BookWork
+	BookWorkID    SqlUUID `gorm:"type:text"`
+	User          User
 	UserID        SqlUUID `gorm:"type:text"`
-	RequestDate   time.Time
+	RequestedDate time.Time
 	FulfilledDate time.Time
-	Status        HoldStatus
+	CancelledDate time.Time
 }
 
 type HoldStatus int
@@ -21,4 +23,41 @@ const (
 	HoldCancelled                        // User canceled hold
 	HoldPostponed                        // User have outstanding charges and cannot check out books right now
 	HoldCompleted                        // User has checked out the book
+	HoldRevoked                          // User account has been deleted
 )
+
+func (h Hold) Status() (HoldStatus, error) {
+	if !h.FulfilledDate.IsZero() {
+		return HoldCompleted, nil
+	}
+
+	if !h.CancelledDate.IsZero() {
+		return HoldCancelled, nil
+	}
+
+	u := h.GetUser()
+	if u.Status == UserStatusDeleted {
+		return HoldRevoked, nil
+	}
+
+	if state, err := u.HasOverdueBooks(); err != nil {
+		return 0, err
+	} else if state {
+		return HoldPostponed, nil
+	}
+
+	if count, err := u.CheckedOut(); err != nil {
+		return 0, err
+	} else if len(count) >= LOAN_LIMIT {
+		return HoldPostponed, nil
+	}
+
+	return HoldQueued, nil
+}
+
+func (h Hold) GetUser() User {
+	if h.User.ID.IsEmpty() {
+		db.Where(&User{BaseModel: BaseModel{ID: h.UserID}}).First(&h.User)
+	}
+	return h.User
+}
