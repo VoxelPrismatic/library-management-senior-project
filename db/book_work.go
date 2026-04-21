244260003458e906886630abb89843d7ba300049
VoxelPrismatic <voxelprismatic@pm.me>
2026-04-21 13:42:21 -0500

    sigh
    Signed-off-by: VoxelPrismatic <voxelprismatic@pm.me>

--- db/book_work.go
+++ db/book_work.go
@@ -99,8 +99,9 @@ func (b *BookWork) Editions() ([]BookWork, error) {
 func (b *BookWork) AllCopies() (CopyList, error) {
 	ret := []BookCopy{}
 	status := db.Joins("book_copies").Where(&BookWork{
-		Title:   b.Title,
-		Authors: b.Authors,
+		Title:    b.Title,
+		Subtitle: b.Subtitle,
+		Authors:  b.Authors,
 	}).Find(&ret)
 
 	return ret, status.Error
@@ -117,24 +118,75 @@ func (b *BookWork) CopiesStrict() (CopyList, error) {
 
 // TO-DO: Fix available copies
 
-
 type CopyCount struct {
 	Total     int
 	Available int
 }
 
-type FormatMap map[string]CopyCount
+type copyCountInter struct {
+	Format BookFmtFlag
+	Count  int
+}
 
-func (b *BookWork) AvailableCopies() FormatMap {
-	copies, _ := b.CopiesStrict()
-	result := FormatMap{}
-	for _, c := range copies {
-		counts := result[string(c.Format)]
-		counts.Total++
-		if c.Status == CopyStatusPublic {
-			counts.Available++
+// Lists available copies
+// If 'strict' is true, then only copies for this particular book ID will be returned
+// If 'strict' is false, then copies matching the title and authors will be returned, too
+func (b *BookWork) AvailableCopies(strict bool) (FormatsMap[CopyCount], error) {
+	ids := []string{b.ID}
+	if !strict {
+		err := db.Where(&BookWork{
+			Title:    b.Title,
+			Subtitle: b.Subtitle,
+			Authors:  b.Authors,
+		}).Pluck("id", &ids).Error
+		if err != nil {
+			return nil, err
 		}
-		result[string(c.Format)] = counts
 	}
-	return result
+
+	var totalCounts []copyCountInter
+	err := db.Model(&BookCopy{}).
+		Where("status = ?", CopyStatusPublic).
+		Where("book_work_id IN ?", ids).
+		Group("format").
+		Select("format, COUNT(*) as count").
+		Scan(&totalCounts).Error
+	if err != nil {
+		return nil, err
+	}
+
+	checkedOutSubquery := db.Table("loands l").
+		Select("1").
+		Where("l.book_copy_id = book_copies.id").
+		Where("l.date_returned = ?", NilTime)
+
+	var availableCounts []copyCountInter
+	err = db.Model(&BookCopy{}).
+		Where("status = ?", CopyStatusPublic).
+		Where("book_work_id IN ?", ids).
+		Where("NOT EXISTS ?", checkedOutSubquery).
+		Group("format").
+		Select("format, COUNT(*) as count").
+		Scan(&availableCounts).Error
+	if err != nil {
+		return nil, err
+	}
+
+	ret := FormatsMap[CopyCount]{}
+	for _, tc := range totalCounts {
+		ret[tc.Format] = CopyCount{Total: tc.Count}
+	}
+	for _, ac := range availableCounts {
+		if c, ok := ret[ac.Format]; ok {
+			c.Available = ac.Count
+			ret[ac.Format] = c
+		} else {
+			ret[ac.Format] = CopyCount{
+				Total:     ac.Count,
+				Available: ac.Count,
+			}
+		}
+	}
+
+	return ret, nil
 }
